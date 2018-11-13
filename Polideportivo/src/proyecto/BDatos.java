69a6f98fe9d3d565693f84563e2da261ef5f6719
Mikel <mikelgonzalez2@gmail.com>
2018-11-13 13:15:08 +0100

    Base de datos funcional :)

--- Polideportivo/src/proyecto/BDatos.java
+++ Polideportivo/src/proyecto/BDatos.java
@@ -6,7 +6,7 @@ public class BDatos {
 	
 public static void main(String[] args) {
 	
-	//Driver se inicializa y registra
+	/*//Driver se inicializa y registra
 	try
 	{
 	   Class.forName("com.mysql.jdbc.Driver");
@@ -16,10 +16,10 @@ public static void main(String[] args) {
 	
 	//Conectamos
 	try {
-		Connection conexion = DriverManager.getConnection ("jdbc:mysql://localhost/prueba","root", "Olatz123gc");
+		Connection conexion = DriverManager.getConnection ("jdbc:mysql://localhost/bdpolideportivo","root", "Olatz123gc");
 		// Preparamos la consulta 
 		Statement s = conexion.createStatement(); 
-		ResultSet rs = s.executeQuery ("select * from usuario");
+		ResultSet rs = s.executeQuery ("select * from usuarios");
 		// Recorremos el resultado, mientras haya registros para leer, y escribimos el resultado en pantalla. 
 		while (rs.next()) 
 		{ 
@@ -33,7 +33,38 @@ public static void main(String[] args) {
 	} catch (SQLException e) {
 		
 		e.printStackTrace();
+	}*/
+	
+	
+	
+	
+	
+	//Conecto con la base 
+	Connection con = null;
+	String sURL = "jdbc:mysql://localhost:3306/bdpolideportivo";
+	try {
+		con = DriverManager.getConnection(sURL,"root","Olatz123gc");
+		System.out.println("Conexión establecida");
+	} catch (SQLException e) {
+		System.out.println("Conexión no establecida, error");
+		
 	}
+	
+	//Hago un statement y lo saco en pantalla 
+	/*try (PreparedStatement stmt = con.prepareStatement("SELECT n_usuario, c_usuario FROM usuarios")) {
+		  ResultSet rs = stmt.executeQuery();
+		 
+		  while (rs.next()) {
+		    System.out.println (rs.getString("n_usuario") + " " + rs.getString("c_usuario"));
+		    
+		  }
+		  
+		} catch (SQLException sqle) { 
+		  System.out.println("Error en la ejecución:" 
+		    + sqle.getErrorCode() + " " + sqle.getMessage());    
+		}*/
+		
+
 	
 	
 }
